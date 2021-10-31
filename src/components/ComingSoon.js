af5523531ee78ed39a5fb1719d217cbfc58ac5be
michaelcjoseph <michael.joseph@hey.com>
2021-10-30 23:30:13 -0500

    Add launch date to coming soon

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -3,6 +3,9 @@ import './ComingSoon.css';
 function ComingSoon() {
   return (
     <div className="coming-soon">
+      <p className="coming-soon-text monospace-font">
+        Launching November 11, 2021
+      </p>
       <div className="cta">
         <a
           className="cta-button"

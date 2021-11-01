d2da0bfeb787f85250cbc0e2ea3d6f9325d460ac
michaelcjoseph <michael.joseph@hey.com>
2021-11-01 18:55:46 -0500

    Clean up unused code

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,10 @@
 import ComingSoon from './ComingSoon.js';
 import Footer from './Footer.js';
-import Mint from './Mint.js';
 import Title from './Title.js';
 
 
 export {
   ComingSoon,
   Footer,
-  Mint,
   Title,
 };

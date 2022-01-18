8aa99beef4ba479484ba34cce204e3ef0714418b
michaelcjoseph <michael.joseph@hey.com>
2022-01-17 20:00:29 -0700

    Calculate all ability stats for an address

--- pages/api/ability/[address].js
+++ pages/api/ability/[address].js
@@ -1,18 +1,81 @@
-import { getSTR } from "../../../util/alchemy"
+import {
+  get6mBlock,
+  getFirstTx,
+  getFromTx,
+  getToTx,
+  getTokens,
+  getNFTCount,
+} from "../../../util/alchemy"
+
+function logCalc(t, logBase, exp) {
+  if (logBase === 2) {
+    return Math.round(Math.log2(Math.pow(t, exp) + 1) + 5)
+  } 
+  
+  return Math.round(Math.log10(Math.pow(t, exp) + 1) + 5)
+}
+
+function getSTR(fromTx) {
+  return logCalc(fromTx.length, 2, 3)
+}
+
+function getDEX(tokens) {
+  const ownedTokens = tokens.filter(({ tokenBalance }) => parseInt(tokenBalance, 16) > 0)
+  return logCalc(ownedTokens.length, 2, 5)
+}
+
+function getCON(toTx) {
+  return logCalc(toTx.length, 2, 3)
+}
+
+function getINT(nftCount) {
+  return logCalc(nftCount, 2, 5)
+}
+
+function getWIS(firstTx, fromTx, toTx) {
+  const latestFromTx = fromTx[fromTx.length - 1]
+  const latestToTx = toTx[toTx.length - 1]
+  const latestTx = (parseInt(latestFromTx.blockNum, 16) > parseInt(latestToTx.blockNum, 16)) ? latestFromTx : latestToTx
+
+  const blockDiff = parseInt(latestTx.blockNum, 16) - parseInt(firstTx.blockNum, 16)
+  return logCalc(blockDiff, 10, 3)
+}
+
+function getCHA(fromTx, toTx) {
+  const toAddresses = fromTx.map(tx => tx.to);
+  const fromAddresses = toTx.map(tx => tx.from);
+
+  const dedupAddresses = [...new Set([...toAddresses, ...fromAddresses])];
+  return logCalc(dedupAddresses.length, 10, 5)
+}
 
 export default async function handler(req, res) {
   const { address } = req.query
+  const firstTx = await getFirstTx(address)
+  const fromBlock = await get6mBlock()
+  const fromTx = await getFromTx(fromBlock, address)
+  const toTx = await getToTx(fromBlock, address)
+  const tokens = await getTokens(address)
+  const nftCount = await getNFTCount(address)
+  
+
+  const STR = getSTR(fromTx)
+  const DEX = getDEX(tokens)
+  const CON = getCON(toTx)
+  const INT = getINT(nftCount)
+  const WIS = getWIS(firstTx, fromTx, toTx)
+  const CHA = getCHA(fromTx, toTx)
 
   const ability = {
-    LVL: null,
-    HP: null,
-    MP: null,
-    STR: await getSTR(address),
-    DEX: null,
-    CON: null,
-    INT: null,
-    WIS: null,
-    CHA: null
+    LVL: Math.round((STR + DEX + CON + INT + WIS + CHA) / 5),
+    HP: 10 * (STR + DEX + CON) + 50,
+    MP: 10 * (INT + WIS + CHA),
+    STR,
+    DEX,
+    CON,
+    INT,
+    WIS,
+    CHA
   }
 
   res.status(200).json({ ability })
--- util/alchemy.js
+++ util/alchemy.js
@@ -2,17 +2,12 @@ function getAlchemyURL() {
   return `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
 }
 
-function logCalc(t, logBase, exp) {
-  if (logBase === 2) {
-    return Math.round(Math.log2(Math.pow(t, exp) + 1) + 5)
-  } 
-  
-  return Math.round(Math.log10(Math.pow(t, exp) + 1) + 5)
+function getAlchemyNFTUrl(address) {
+  return `https://eth-mainnet.g.alchemy.com/${process.env.ALCHEMY_API_KEY}/v1/getNFTs/?owner=${address}`
 }
 
 async function fetchAlchemyData(method, params) {
   try {
-    console.log(getAlchemyURL())
     const alchemyRes = await fetch(getAlchemyURL(), {
       method: 'POST',
       headers: {
@@ -20,6 +15,7 @@ async function fetchAlchemyData(method, params) {
       },
       body: JSON.stringify({
         "jsonrpc": "2.0",
+        "id": 0,
         "method": method,
         "params": params,
       })
@@ -32,21 +28,98 @@ async function fetchAlchemyData(method, params) {
   }
 }
 
-async function getSTR(address) {
+async function get6mBlock() {
+  try {
+    const res = await fetchAlchemyData('eth_getBlockByNumber', ['latest', false])
+    const { number } = res.result
+    return `0x${(parseInt(number, 16) - 1000000).toString(16)}`
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getFirstTx(address) {
+  try {
+    const resFrom = await fetchAlchemyData('alchemy_getAssetTransfers', [{
+      "fromBlock": "0x1",
+      "fromAddress": address,
+    }])
+
+    const resTo = await fetchAlchemyData('alchemy_getAssetTransfers', [{
+      "fromBlock": "0x1",
+      "toAddress": address,
+    }])
+
+    const firstFromTx = resFrom.result.transfers[0]
+    const firstToTx = resTo.result.transfers[0]
+
+    if (parseInt(firstFromTx, 16) > parseInt(firstToTx, 16)) {
+      return firstToTx
+    }
+
+    return firstFromTx
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+async function getFromTx(fromBlock, address) {
   try {
     const res = await fetchAlchemyData('alchemy_getAssetTransfers', [{
-      "fromBlock": "0xC30965",
+      "fromBlock": fromBlock,
       "toBlock": "latest",
       "fromAddress": address,
     }])
 
-    return logCalc(res.result.transfers.length, 2, 3)
+    return res.result.transfers
   } catch (error) {
    console.log(error)
    return 0 
   }  
 }
 
+async function getToTx(fromBlock, address) {
+  try {
+    const res = await fetchAlchemyData('alchemy_getAssetTransfers', [{
+      "fromBlock": fromBlock,
+      "toBlock": "latest",
+      "toAddress": address,
+    }])
+
+    return res.result.transfers
+  } catch (error) {
+   console.log(error)
+   return 0 
+  }  
+}
+
+async function getTokens(address) {
+  try {
+    const res = await fetchAlchemyData('alchemy_getTokenBalances', [address, 'DEFAULT_TOKENS'])
+    return res.result.tokenBalances
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+async function getNFTCount(address) {
+  try {
+    const res = await fetch(getAlchemyNFTUrl(address))
+    const json = res.json()
+    return json.ownedNfts.totalCount
+  } catch (error) {
+    console.log(error)
+    return 0
+  }
+}
+
 export {
-  getSTR
+  get6mBlock,
+  getFirstTx,
+  getFromTx,
+  getToTx,
+  getTokens,
+  getNFTCount
 }
